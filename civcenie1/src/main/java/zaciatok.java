f2f00a6396e2a9f7f914aa83b0c5d43545afa761
lukasnovak12 <xnovakl1@stuba.sk>
2022-02-16 16:51:06 +0100

    initial commit

---
+++ civcenie1/src/main/java/zaciatok.java
@@ -0,0 +1,7 @@
+public class zaciatok {
+    public static void main(String[] args){
+        int cislo= 5;
+        int cislo2 = 10;
+        System.out.println(cislo+cislo2);
+    }
+}
--- cvicenie1/src/main/java/ggh.java
+++ cvicenie1/src/main/java/ggh.java
@@ -1,2 +1,5 @@
 public class ggh {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
 }
